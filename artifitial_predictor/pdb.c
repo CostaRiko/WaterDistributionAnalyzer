05bb50950b2bc4761dd9c56c0cb2fe1c3676b82f
Андрей Беспалов <a.bespalov@g.nsu.ru>
2020-08-30 17:36:42 +0700

    Substring function is added. String comparer is added. Convertation pdb line to correspinding data types is added.

--- artifitial_predictor/pdb.c
+++ artifitial_predictor/pdb.c
@@ -1,6 +1,6 @@
 #include "pdb.h"
 
-char* reading_buffer; // Память не возвращена системе!!!
+char* reading_buffer;
 DWORD file_size;
 
 BOOL read_pdb_file(wchar_t* path)
@@ -29,7 +29,7 @@ BOOL proceed_pdb_data()
 	ZeroMemory(line, 128);
 	for (int i = 0; i < file_size; i++) {
 		if (reading_buffer[i] == '\n') {
-			proceed_pdb_line(line);
+			proceed_pdb_line(line, 128);
 			ZeroMemory(line, 128);
 			line_cursor = 0;
 		}
@@ -42,10 +42,69 @@ BOOL proceed_pdb_data()
 	return TRUE;
 }
 
-BOOL proceed_pdb_line(char* line)
+BOOL proceed_pdb_line(char* line, const int linelen)
 {
+	int 
+		id, resSeq, substring_status; 
 
+	double 
+		x, y, z; 
+
+	char 
+		idc[6], name[4], res[6], record[6], resSeqc[4], xc[8], yc[8], zc[8];
+
+	ZeroMemory(idc, 6); ZeroMemory(name, 4); ZeroMemory(res, 6); ZeroMemory(record, 6);
+	ZeroMemory(resSeqc, 4); ZeroMemory(xc, 4); ZeroMemory(yc, 5);  ZeroMemory(zc, 6);
+
+	substring_status = substring(line, 7,  12, linelen, idc, 6);
+	substring_status = substring(line, 0, 7, linelen, record, 6);
+	substring_status = substring(line, 13, 17, linelen, name, 4);
+	substring_status = substring(line, 17, 21, linelen, res,6);
+	substring_status = substring(line, 23, 27, linelen, resSeqc, 4);
+	substring_status = substring(line, 31, 38, linelen, xc, 8);
+	substring_status = substring(line, 38, 46, linelen, yc, 8);
+	substring_status = substring(line, 46, 55, linelen, zc, 8);
+
+	if ( ! str_is(record, 6, "ATOM  ", 6)) {
+		return FALSE;
+	}
+	id = atoi(idc);
+	resSeq = atoi(resSeqc);
+	x = atof(xc);
+	y = atof(yc);
+	z = atof(zc);
+	name[3] = '\0';
+	res[5] = '\0';
+	printf("ID: %i %s %s %i %lf %lf %lf\r", id, name, res, resSeq, x, y, z);
 	return TRUE;
+}
+
+BOOL str_is(char* str1, int str1len, const char* str2, int str2len)
+{
+	if (str1len == str2len) {
+		for (int i = 0; i < str1len; i++) {
+			if (str1[i] != str2[i]) {
+				return FALSE;
+			}
+		}
+	}
+	else {
+		return FALSE;
+	}
+	return TRUE;
+}
+
+int substring(char* str, int start, int finish, int linelen, char * result, int result_len)
+{
+	int result_cursor = 0;
+	for (int i = start; i < finish; i++) {
+		if (result_cursor >= result_len) {
+			return 1;
+		}
+		result[result_cursor] = str[i];
+		result_cursor++;
+	}
+	return 0;
 }
 
 
--- artifitial_predictor/pdb.h
+++ artifitial_predictor/pdb.h
@@ -3,4 +3,6 @@
 
 BOOL read_pdb_file(wchar_t * path);
 BOOL proceed_pdb_data();
-BOOL proceed_pdb_line(char* line);
+BOOL proceed_pdb_line(char* line, const int linelen);
+BOOL str_is(char* str1, const char* str2);
+int substring(char* str, int start, int finish, int linelen, char* result, int result_len);
